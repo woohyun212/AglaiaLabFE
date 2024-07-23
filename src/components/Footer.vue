509807b5ffe9eb87df7666fa2cfe1894ceeae086
woohyun212 <woohyun212@gmail.com>
2024-07-23 16:29:11 +0900

    Combine Projects

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer>
+    <p>&copy; 2024 Aglaia Lab</p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+footer {
+  background-color: #333;
+  color: white;
+  text-align: center;
+  padding: 10px 0;
+  margin-top: auto; /* Ensure footer sticks to the bottom if content is shorter than the viewport */
+}
+</style>
