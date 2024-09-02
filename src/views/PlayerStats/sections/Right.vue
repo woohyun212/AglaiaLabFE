c13bd83627be89eaeed17ab8418ab60f87c113a4
woohyun212 <woohyun212@gmail.com>
2024-09-02 13:58:13 +0900

    🧹 Cleanup: Removed unnecessary files to streamline the project.

--- src/views/PlayerStats/sections/Right.vue
+++ src/views/PlayerStats/sections/Right.vue
@@ -4,34 +4,21 @@
     <div class="recent-match-stats">
       <RecentMatchHistoryOverview/>
       <MatchHistoryCard/>
-<!---->
-<!--      <Element3/>-->
-<!--      <Element4/>-->
-<!--      <Element5/>-->
-<!--      <View6/>-->
     </div>
   </div>
 </template>
-<!--!TODO: 동일한 색상은 styleguide에 변수로 등록하기-->
+
 <script>
 import MatchTypeTab from "./Right/sections/MatchTypeTab.vue";
 import RecentMatchHistoryOverview from "./Right/sections/RecentMatchHistoryOverview.vue";
 import MatchHistoryCard from "./Right/sections/MatchHistoryCard.vue";
-import Element3 from "./Right/sections/silver.vue";
-import Element4 from "./Right/sections/bronze.vue";
-import Element5 from "./Right/sections/else.vue";
-import View6 from "./Right/sections/escape.vue";
 
 export default {
   name: "Right",
   components: {
     MatchTypeTab,
     RecentMatchHistoryOverview,
-    MatchHistoryCard,
-    Element3,
-    Element4,
-    Element5,
-    View6,
+    MatchHistoryCard
   },
 };
 </script>
